fbfdc3d9233e35d32462b9b4b7e39228ea17448e
mathiasprojectexe <82294610+mathiasprojectexe@users.noreply.github.com>
2021-12-11 22:11:26 +0100

    Update animeunitypro.js

--- animeunitypro.js
+++ animeunitypro.js
@@ -1,7 +1,7 @@
 console.log('bottone creato');
 var skipBtn = document.createElement('button');
-skipBtn.class = 'plyr__controls__item plyr__control';
-skipBtn.type = 'button';
-skipBtn.id = 'skipbtnAnimePro';
+skipBtn.setAttribute('class','plyr__controls__item plyr__control');
+skipBtn.setAttribute('type', 'button');
+skipBtn.setAttribute('id', 'skipbtnAnimePro');
 document.getElementsByClassName('plyr__controls')[0].appendChild(skipBtn);
 
