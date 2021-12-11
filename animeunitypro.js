4429914dd4e83deceb0961e2a358aa64cfb93886
mathiasprojectexe <82294610+mathiasprojectexe@users.noreply.github.com>
2021-12-11 22:02:18 +0100

    Create animeunitypro.js

---
+++ animeunitypro.js
@@ -0,0 +1,12 @@
+function createSkipBtn(){
+    console.log('bottone creato');
+    var skipBtn = document.createElement('button');
+    skipBtn.class = 'plyr__controls__item plyr__control';
+    skipBtn.type = 'button';
+    skipBtn.id = 'skipbtnAnimePro';
+    document.getElementsByClassName('plyr__controls')[0].appendChild(skipBtn);
+}
+
+
+createSkipBtn();
+
